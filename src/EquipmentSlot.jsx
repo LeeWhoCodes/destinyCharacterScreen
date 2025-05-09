744795ca1dff62dd32521aa43384a44e5138fed1
LeeWhoCodes <leewhocodes@gmail.com>
2025-05-09 15:13:01 +1200

    created EquipmentSlot component, so far a simple square with an image inside

---
+++ src/EquipmentSlot.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function EquipementSlot() {
+
+  return (
+    <div className="border-2 border-yellow-400 bg-purple-600 size-1/2 m-auto h-full overflow-hidden">
+        <img className='-translate-y-3' src="../public/LeeHeadphones_BGR.png" alt="" />
+    </div>
+  )
+}
+
+export default EquipementSlot
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,26 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
+
+import EquipementSlot from './EquipmentSlot'
 
 function Grid() {
 
   return (
     <div className="grid grid-cols-5 grid-rows-6 gap-3 px-50 py-25 h-screen">
-        <div className="outline">4</div>
-        <div className="col-start-1 row-start-2 outline">5</div>
-        <div className="col-start-1 row-start-3 outline">6</div>
-        <div className="col-start-1 row-start-4 outline">7</div>
-        <div className="col-start-1 row-start-5 outline">8</div>
-        <div className="col-start-1 row-start-6 outline">9</div>
-        <div className="col-start-5 row-start-1 outline">10</div>
-        <div className="col-start-5 row-start-2 outline">11</div>
-        <div className="col-start-5 row-start-3 outline">12</div>
-        <div className="col-start-5 row-start-4 outline">13</div>
-        <div className="col-start-5 row-start-5 outline">14</div>
-        <div className="col-start-5 row-start-6 outline">15</div>
-        <div className="col-start-2 row-start-1 outline">16</div>
-        <div className="row-span-4 col-start-4 row-start-1 outline">17</div>
+        <div className="outline">
+            <p className='absolute'>1</p>
+        </div>
+        <div className="col-start-1 row-start-2 outline">
+            <p className='absolute'>2</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-1 row-start-3 outline">
+            <p className='absolute'>3</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-1 row-start-4 outline">
+            <p className='absolute'>4</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-1 row-start-5 outline">
+            <p className='absolute'>5</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-1 row-start-6 outline">
+            <p className='absolute'>6</p>
+        </div>
+        <div className="col-start-5 row-start-1 outline">
+            <p className='absolute'>7</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-5 row-start-2 outline">
+            <p className='absolute'>8</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-5 row-start-3 outline">
+            <p className='absolute'>9</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-5 row-start-4 outline">
+            <p className='absolute'>10</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-5 row-start-5 outline">
+            <p className='absolute'>11</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-5 row-start-6 outline">
+            <p className='absolute'>12</p>
+            <EquipementSlot />
+        </div>
+        <div className="col-start-2 row-start-1 outline">
+            <p className='absolute'>13</p>
+        </div>
+        <div className="row-span-4 col-start-4 row-start-1 outline">
+            <p className='absolute'>14</p>
+        </div>
     </div>
   )
 }
