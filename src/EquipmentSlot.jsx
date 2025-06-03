bf67d5c045b2bb176868a15c523faf70eef64674
LeeWhoCodes <leewhocodes@gmail.com>
2025-06-03 17:51:41 +1200

    info card positened based on location on screen

--- src/EquipmentSlot.jsx
+++ src/EquipmentSlot.jsx
@@ -3,28 +3,67 @@ import { useRef } from 'react'
 
 import InventoryGrid from './InventoryGrid';
 
-import { useFloating } from '@floating-ui/react';
+import {
+    useFloating,
+    autoUpdate,
+    offset,
+    shift,
+} from '@floating-ui/react';
 
 import './index.css'
 
 function EquipementSlot({slot}) {
 
-  const equipedSlotRef = useRef(null);
-  const inventoryRef = useRef(null);
+  const {
+    refs: invRefs,
+    floatingStyles: invFloatingStyles,
+    } = useFloating ({
+        placement: 'left-start',
+        whileElementsMounted: autoUpdate,
+    });
 
-  const {refs, floatingStyles} = useFloating();
+    const {
+        refs: infoRefs,
+        floatingStyles: infoFloatingStyles,
+    } = useFloating ({
+      placement: 'right',
+      whileElementsMounted: autoUpdate,
+      middleware: [
+        offset(10),
+        shift({
+          padding: 5,
+        }),
+      ],
+    })
+
+  const {refs, floatingStyles} = useFloating({
+    placement: 'left-start',
+    whileElementsMounted: autoUpdate,
+  });
+
+  // const ref = useMergeRefs([invRefs.setReference, infoRefs.setReference]);
 
   return (
-    <div className="border-2 border-yellow-400 bg-purple-600 size-1/2 m-auto h-full overflow-hidden" ref={refs.setReference} onMouseEnter={() => inventoryRef.current.showPopover()} onMouseLeave={() => inventoryRef.current.hidePopover()}>
-        <button className='bg-blue-400' popoverTarget='popover-primary-inventory'>
-          popover
-        </button>
-            <div className='outline' ref={refs.setFloating} style={floatingStyles} id='popover-primary-inventory' popover="auto">
+    <div className="border-2 border-yellow-400 bg-purple-600 size-1/2 m-auto h-full overflow-hidden" ref={invRefs.setReference} onMouseEnter={() => invRefs.floating.current.showPopover()} onMouseLeave={() => invRefs.floating.current.hidePopover()}>
+      <div ref={infoRefs.setReference} onMouseEnter={() => infoRefs.floating.current.showPopover()} onMouseLeave={() => infoRefs.floating.current.hidePopover()}>
+          <button className='bg-blue-400' >
+            popover
+          </button>
+            <div className='outline' ref={invRefs.setFloating} style={invFloatingStyles} popover="manual">
                 {/* <p>This is where the {slot} inventory would go</p> */}
                 <InventoryGrid />
-            </div>
-        
-        <img className='-translate-y-3' src="/LeeHeadphones_BGR.png" alt="" />
+            </div>        
+          <img className='-translate-y-3' src="/LeeHeadphones_BGR.png" alt="" />
+        </div>
+        <div className='outline bg-amber-400 h-100%' ref={infoRefs.setFloating} style={infoFloatingStyles} popover="manual">
+          <InventoryGrid />
+          <InventoryGrid />
+          <InventoryGrid />
+          <InventoryGrid />
+          <InventoryGrid />
+          <InventoryGrid />
+          <InventoryGrid />
+        </div>
     </div>
   )
 }
